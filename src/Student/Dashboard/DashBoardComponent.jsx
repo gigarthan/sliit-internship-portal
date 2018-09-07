54611182d52c845f7fa2d9ad4462e0b9619a4696
gigarthan <gigajpx@gmail.com>
2018-09-07 11:51:59 +0530

    add student dashboard

--- src/Student/Dashboard/DashBoardComponent.jsx
+++ src/Student/Dashboard/DashBoardComponent.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from 'react'
-import { Grid, Button } from 'semantic-ui-react';
+import { Container, Grid, Button } from 'semantic-ui-react';
 import StudentProfile from './StudentProfileComponent';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
+import RegisterForm from './RegisterFormComponent';
 
 export default class DashBoard extends Component {
   render() {
     return (
-      <div>
-        <Grid  >
+      <Container fluid>
+        <Grid style={{padding:50}}>
            <Grid.Row columns={2} >
-                <Grid.Column>
+                <Grid.Column width={4}>
                     <StudentProfile />
                 </Grid.Column>
-                <Grid.Column>
-                    <Link to='/student/internship/begin' ><Button> Begin Internship </Button>    </Link>
+                <Grid.Column width={6}>                   
+                    <Route   path={`${this.props.match.url}/internship/begin`} component={RegisterForm} />
+                    <Route exact path={`${this.props.match.url}`}  render={() => { return <Link to={`${this.props.match.url}/internship/begin`} ><Button> Begin Internship </Button></Link> }} />
                 </Grid.Column> 
            </Grid.Row>
         </Grid>
-      </div>
+      </Container>
     )
   }
 }
