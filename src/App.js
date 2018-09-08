9603f9b10fb0920c8294f81bf6a1810d72c9a883
marjanmukram <marjanmukram8@gmail.com>
2018-09-08 10:44:19 +0530

    Company Form button To display  added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Button, Input, Form, Message } from 'semantic-ui-react'
+import FormCompany from './form_company'
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isCompanyFormClicked : false
+    }
+    this.funcFormCompany = this.funcFormCompany.bind(this);
+  }
+  funcFormCompany(){
+    this.setState({
+      isCompanyFormClicked : !this.state.isCompanyFormClicked});
+}
   render() {
+ 
     return (
       <div className="App">
         <h1>Internship Portal</h1>
+        <Button onClick={this.funcFormCompany}>To be filled by the company</Button>
+         <div className="FormCompany">
+         { this.state.isCompanyFormClicked? <FormCompany/>:null}
+        </div> 
       </div>
     );
   }
