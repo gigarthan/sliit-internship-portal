d20b3a1aa4e19fd386e02ab53b2e66e79c6c74b2
kiru07 <kirushanthan007@gmail.com>
2018-09-08 20:42:40 -0700

    Added dummy data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import CardExampleCard from './SupervisorAllocator/CardExampleCard';
 
 
 class App extends Component {
+  state = {
+    companies: [
+      { id: 1, name: "Softlogic" },
+      { id: 2, name: "Hardlogic" },
+      { id: 3, name: "99x" }
+    ],
+    supervisors: [
+      { id: 1, name: "Peter", title: "UX", email: "peter@gmail.com", companyId: 1, allocatedInterns: [] },
+      { id: 5, name: "Tom", title: "SE", email: "tom@gmail.com", companyId: 1, allocatedInterns: [] },
+      { id: 7, name: "John", title: "SE", email: "john@gmail.com", companyId: 3, allocatedInterns: [] }
+
+    ],
+    interns: [
+      { id: 1, name: "Max", position: "SE", supervisor: {}, companyId: 1 },
+      { id: 1, name: "Max", position: "SE", supervisor: {}, companyId: 1 },
+      { id: 1, name: "Max", position: "SE", supervisor: {}, companyId: 1 },
+      { id: 1, name: "Max", position: "SE", supervisor: {}, companyId: 3 },
+      { id: 1, name: "Max", position: "SE", supervisor: {}, companyId: 3 }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <CardExampleCard />
+        <CardExampleCard name="Max" />
       </div>
     );
   }
--- src/SupervisorAllocator/CardExampleCard.jsx
+++ src/SupervisorAllocator/CardExampleCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 
-const CardExampleCard = () => (
+const CardExampleCard = (props) => (
     <Card>
         <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
         <Card.Content>
-            <Card.Header>Matthew</Card.Header>
+            <Card.Header>{props.name}</Card.Header>
             <Card.Meta>
                 <span className='date'>Joined in 2015</span>
             </Card.Meta>
