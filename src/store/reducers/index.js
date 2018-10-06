f011bac2bd4d8aceffc29b3584358cdb259c14ef
kiru <kirushanthan007@gmail.com>
2018-10-06 16:22:55 -0700

    fixed imports

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import { firebaseReducer } from 'react-redux-firebase';
-import { allocationReducer } from './allocationReducer';
+import allocationReducer from './allocationReducer';
 
 const rootReducer = combineReducers({
     form: formReducer,
     firebase: firebaseReducer,
-    allocation: allocationReducer
+    allocationReducer
 });
 
 export default rootReducer;
